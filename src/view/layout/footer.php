10900a3946730dabac7f55e17276776c3173c292
Yampiers Quispe <169065995+Jhampiers@users.noreply.github.com>
2025-11-06 15:48:53 -0500

    init

--- src/view/layout/footer.php
+++ src/view/layout/footer.php
@@ -1,6 +1,6 @@
 </div>
 <footer style="background:#0b1220;color:#fff;padding:12px 20px;margin-top:24px">
-  <small>Sistema de Canchas Deportivas</small>
+  <small>Sistema de prueba</small>
 </footer>
 </main>
 </div>
